5f0b9ce258fed31094d29f6c544c70c4653a98dd
Nam Trần <nhatnam99132@gmail.com>
2020-06-08 20:14:09 +0700

    add jquey

---
+++ Lab4-5-6/Controllers/CoursesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Lab4_5_6.Models;
+
+namespace Lab4_5_6.Controllers
+{
+    internal class CoursesViewModel
+    {
+        public IQueryable<Course> UpcomingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
